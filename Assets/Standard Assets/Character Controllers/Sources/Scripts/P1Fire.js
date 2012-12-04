8cbf118a3bb4363c3c00359a2b3b65ed075ad656
Franziska Zeiner <franziska.zeiner@gmail.com>
2012-12-04 00:58:34 -0500

    P1 and P2 can shoot

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/P1Fire.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/P1Fire.js
@@ -2,6 +2,6 @@ var fireBall : GameObject;
 var shootPoint : Transform;
 
  function Update (){
-        if(Input.GetKeyDown("E")){
+        if(Input.GetKeyDown("e")){
        var shot : GameObject = Instantiate(fireBall, shootPoint.position, shootPoint.rotation);
 shot.GetComponent(Rigidbody).velocity = 100.0 * shootPoint.forward; }}
