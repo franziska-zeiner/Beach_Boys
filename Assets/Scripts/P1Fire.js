5542adc7591d205e89c44d1ff5065ea69880b74e
Robert Scott Dionne <robertsdionne@gmail.com>
2012-12-06 09:27:25 -0500

    Add fire and jump controls to the joysticks.

--- Assets/Scripts/P1Fire.js
+++ Assets/Scripts/P1Fire.js
@@ -1,7 +1,9 @@
 var fireBall : GameObject;
 var shootPoint : Transform;
 
- function Update (){
-        if(Input.GetKeyDown("e")){
-       var shot : GameObject = Instantiate(fireBall, shootPoint.position, shootPoint.rotation);
-shot.GetComponent(Rigidbody).velocity = 100.0 * shootPoint.forward; }}
+function Update () {
+  if(Input.GetButton("Fire P1")) {
+    var shot : GameObject = Instantiate(fireBall, shootPoint.position, shootPoint.rotation);
+    shot.GetComponent(Rigidbody).velocity = 100.0 * shootPoint.forward;
+  }
+}
--- Assets/Scripts/P2Fire.js
+++ Assets/Scripts/P2Fire.js
@@ -1,7 +1,9 @@
 var fireBall : GameObject;
 var shootPoint : Transform;
 
- function Update (){
-        if(Input.GetKeyDown("f")){
-       var shot : GameObject = Instantiate(fireBall, shootPoint.position, shootPoint.rotation);
-shot.GetComponent(Rigidbody).velocity = 100.0 * shootPoint.forward; }}
+function Update () {
+  if(Input.GetButton("Fire P2")) {
+    var shot : GameObject = Instantiate(fireBall, shootPoint.position, shootPoint.rotation);
+    shot.GetComponent(Rigidbody).velocity = 100.0 * shootPoint.forward;
+  }
+}
