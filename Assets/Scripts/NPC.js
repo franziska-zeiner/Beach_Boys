da3cec252a8023fd7f36cc2e6a401d0840fc0544
Robert Scott Dionne <robertsdionne@gmail.com>
2012-12-19 01:40:06 -0500

    Add Zeke's audio to the game!

--- Assets/Scripts/NPC.js
+++ Assets/Scripts/NPC.js
@@ -1,15 +1,21 @@
 #pragma strict
 
 var animator : Animator;
+var audioClips : AudioClip[];
 var paceDuration : float;
 var speed : float;
 
+private var audioSource : AudioSource;
 private var controller: CharacterController;
 private var lastPaceTime : float;
 
 function Start () {
+  audioSource = GetComponent(AudioSource);
   controller = GetComponent(CharacterController);
   lastPaceTime = Time.fixedTime - paceDuration * Random.value;
+  audioSource.clip = audioClips[Random.Range(0, 23)];
+  audioSource.time = Random.Range(0, audioSource.clip.length);
+  audioSource.Play();
 }
 
 function Update () {
