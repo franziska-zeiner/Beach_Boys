f3d8418d24527c7968ba0f7d3aa6618a6680ef3b
Robert Scott Dionne <robertsdionne@gmail.com>
2012-12-19 18:50:59 -0500

    Make the dialogue sparser with random delays.

--- Assets/Scripts/NPC.js
+++ Assets/Scripts/NPC.js
@@ -2,20 +2,23 @@
 
 var animator : Animator;
 var audioClips : AudioClip[];
+var minSpeakDelay : float;
+var maxSpeakDelay : float;
 var paceDuration : float;
 var speed : float;
 
 private var audioSource : AudioSource;
 private var controller: CharacterController;
 private var lastPaceTime : float;
+private var lastSpeakTime : float;
+private var speakDelay : float;
 
 function Start () {
   audioSource = GetComponent(AudioSource);
   controller = GetComponent(CharacterController);
   lastPaceTime = Time.fixedTime - paceDuration * Random.value;
-  var clipIndex : int = Random.Range(0, 23);
-  var clipTime : float = Random.value;
-  npcPlaySound(clipIndex, clipTime);
+  lastSpeakTime = Time.fixedTime;
+  speakDelay = Random.Range(minSpeakDelay, maxSpeakDelay);
 }
 
 function npcPlaySound(index : int, time : float) {
@@ -29,6 +32,15 @@ function Update () {
     if (Time.fixedTime - lastPaceTime > paceDuration) {
       transform.Rotate(180.0 * Vector3.up);
       lastPaceTime = Time.fixedTime;
+    }
+    if (!audioSource.isPlaying && Time.fixedTime - lastSpeakTime > speakDelay) {
+	  var clipIndex : int = Random.Range(0, 23);
+	  var clipTime : float = Random.value;
+	  npcPlaySound(clipIndex, clipTime);
+	  lastSpeakTime = Time.fixedTime;
+	  speakDelay = Random.Range(minSpeakDelay, maxSpeakDelay);
+    } else if (Time.fixedTime - lastSpeakTime > speakDelay) {
+      lastSpeakTime = Time.fixedTime;
     }
     controller.SimpleMove(speed * transform.forward);
     npcSetSpeed();
