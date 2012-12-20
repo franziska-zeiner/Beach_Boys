0c6e938c79289a364d92fcabeb0f9c755c68e2d5
Robert Scott Dionne <robertsdionne@gmail.com>
2012-12-20 09:03:55 -0500

    Remove unused shoot script.

--- Assets/Scripts/Shoot.js
+++
@@ -1,9 +0,0 @@
-#pragma strict
-
-var fireBall : GameObject;
-
-@RPC
-function Shoot(position : Vector3, orientation : Quaternion, velocity : Vector3) {
-  var shot : GameObject = Network.Instantiate(fireBall, position, orientation, 0);
-  shot.GetComponent(Rigidbody).velocity = velocity;
-}
